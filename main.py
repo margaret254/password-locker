328cb29ce6c743b1b21d2d4e44d8280fe78ec042
margaret254 <maggiemwas91@gmail.com>
2019-11-08 16:40:07 +0300

    Create class User and  its instance variables in main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+class User:
+    '''
+    Class that generates new instances of users
+    '''
+
+    user_list = [] # Empty User List
+
+    def __init__(self,first_name,last_name,phone_number,email,password):
+        '''
+        __init__ method that helps us define properties for our objects
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
+        self.password = password
+
+
--- test.py
+++ test.py
@@ -1,4 +1,27 @@
 import unittest 
 from main import User
-from main import Crediantials
+from main import Credentials
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+    '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Margaret","Maggie@254")
+        # create user object
+
+    def test_init(self):
+        '''
+        test_init to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.user_name,"Margaret")
+        self.assertEqual(self.new_user.password,"Maggie@254")
+
+if __name__=='__main__':
+    unittest.main()
 
