309640db7e5019c6548479708b6034140a22e8f2
margaret254 <maggiemwas91@gmail.com>
2019-11-08 16:49:01 +0300

    Add test_init function in the test.py file

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import unittest 
 from main import User
-from main import Credentials
 
 class TestUser(unittest.TestCase):
 
@@ -12,15 +11,18 @@ class TestUser(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_user = User("Margaret","Maggie@254")
+        self.new_user = User("Margaret","Mwangi","07123456789","Maggie@254","1234")
         # create user object
 
     def test_init(self):
         '''
         test_init to test if the object is initialized properly
         '''
-        self.assertEqual(self.new_user.user_name,"Margaret")
-        self.assertEqual(self.new_user.password,"Maggie@254")
+        self.assertEqual(self.new_user.first_name,"Margaret")
+        self.assertEqual(self.new_user.last_name,"Mwangi")
+        self.assertEqual(self.new_user.phone_number,"0712345689")
+        self.assertEqual(self.new_user.email,"Maggie@254")
+        self.assertEqual(self.new_user.password,"1234")
 
 if __name__=='__main__':
     unittest.main()
