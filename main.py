3055bb65c3c50ac58abff51f8ac35106f8502b34
margaret254 <maggiemwas91@gmail.com>
2019-11-10 22:47:18 +0300

    Add a useername value

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class User:
 
     user_list = [] # Empty User List
 
-    def __init__(self,first_name,last_name,phone_number,email,password):
+    def __init__(self,first_name,last_name,phone_number,email,user_name,password):
         '''
         __init__ method that helps us define properties for our objects
         '''
@@ -17,6 +17,7 @@ class User:
         self.last_name = last_name
         self.phone_number = phone_number
         self.email = email
+        self.user_name = user_name
         self.password = password
     
     def save_user(self):
