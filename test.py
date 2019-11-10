ce8151cfaaab5adc092b1c273113121aedaad095
margaret254 <maggiemwas91@gmail.com>
2019-11-10 22:47:34 +0300

    Add a username value

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ class TestUser(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_user = User("Margaret","Mwangi","07123456789","Maggie@254","1234")
+        self.new_user = User("Margaret","Mwangi","07123456789","Maggie@254","serah","1234")
         # create user object
 
     def test_init(self):
@@ -24,6 +24,7 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.last_name,"Mwangi")
         self.assertEqual(self.new_user.phone_number,"07123456789")
         self.assertEqual(self.new_user.email,"Maggie@254")
+        self.assertEqual (self.new_user.user_name,"serah")
         self.assertEqual(self.new_user.password,"1234")
 
     def test_save_user(self):
@@ -47,7 +48,7 @@ class TestUser(unittest.TestCase):
         '''
 
         self.new_user.save_user()
-        test_user = User("Margaret","Mwangi","07123456789","Maggie@254","1234") #new user
+        test_user = User("Margaret","Mwangi","07123456789","Maggie@254","serah","1234") #new user
         test_user.save_user()
 
         self.new_user.delete_user() #Deleting a user
@@ -64,13 +65,13 @@ class TestCredential(unittest.TestCase):
         '''
         setup method to run before each test cases
         '''
-        self.new_credential = Credentials("maggie","facebook","1234") #Create Credential object
+        self.new_credential = Credentials("serah","facebook","1234") #Create Credential object
 
     def test_init(self):
         '''
         To test if the object is initialized properly
         '''
-        self.assertEqual(self.new_credential.user_name,"maggie")
+        self.assertEqual(self.new_credential.user_name,"serah")
         self.assertEqual(self.new_credential.account_name,"facebook")
         self.assertEqual(self.new_credential.password,"1234")
 
@@ -84,7 +85,7 @@ class TestCredential(unittest.TestCase):
         '''
 
         self.new_credential.save_credentials() # Saving the new credentials
-        facebook = Credentials('maggie','facebook','1234')
+        facebook = Credentials('serah','facebook','1234')
         facebook.save_credentials()
         self.assertEqual(len(Credentials.credential_list),2)
 
@@ -94,7 +95,7 @@ class TestCredential(unittest.TestCase):
         '''
 
         self.new_credential.save_credentials()
-        facebook = Credentials('maggie','facebook','1234')
+        facebook = Credentials('serah','facebook','1234')
         facebook.save_credentials()
         facebook.delete_credentials()
         self.assertEqual(len(Credentials.credential_list),1)
@@ -105,7 +106,7 @@ class TestCredential(unittest.TestCase):
         '''
 
         self.new_credential.save_credentials()
-        facebook = Credentials('maggie','facebook','1234')
+        facebook = Credentials('serah','facebook','1234')
         facebook.save_credentials()
         credential_found = Credentials.find_by_account_name('facebook')
         self.assertEqual(credential_found.account_name,'facebook')
@@ -122,7 +123,7 @@ class TestCredential(unittest.TestCase):
         '''
 
         self.new_credential.save_credentials()
-        facebook = Credentials('maggie','facebook','1234')
+        facebook = Credentials('serah','facebook','1234')
         facebook.save_credentials()
         found_credential = None
         for credential in Credentials.credential_list:
